17d5fea50ab134711e293c3214e92def3171cd12
Vani-code24 <70517782+Vani-code24@users.noreply.github.com>
2022-04-21 11:25:54 +0530

    Delete Tasks1/src/JS directory

--- Tasks1/src/JS/script.js
+++
@@ -1,8 +0,0 @@
-const ham = document.querySelector(".hamburger"); 
-const navbar= document.querySelector(".header__list");
-const navicon= document.querySelector(".header__icons"); 
-ham.addEventListener('click',()=>{
-    ham.classList.toggle("change")
-    navbar.classList.toggle("nav-change")
-    navicon.classList.toggle("nav-change")
-})
